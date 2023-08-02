1fd80b1d7c086b42799a4218e4421b790ee78fb2
Luna0225 <132825841+Luna0225@users.noreply.github.com>
2023-08-02 23:50:03 +0900

    各種変更

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,26 +5,29 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Range(0, 10)] private float moveSpeed = 3f; //移動速度
+    [SerializeField, Range(0, 10)] private float moveSpeed = 5f; //移動速度
     [SerializeField, Range(0, 10)] private float jumpPower = 3f;　//ジャンプ力
     private CharacterController characterController;  //CharacterControllerのキャッシュ
 
     private Transform cachedtransform; //transformのキャッシュ
     private Vector3 moveVelocity; //キャラクターの移動速度の情報
 
+    private Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>(); //負荷を下げるためにキャッシュしておく
         cachedtransform = transform;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log("マウス座標:" + Input.mousePosition);
-        Debug.Log(characterController.isGrounded ? "地上にいます" : "空中にいます");
+        //Debug.Log(characterController.isGrounded ? "地上にいます" : "空中にいます");
         //入力軸による移動処理(完成を無視しているのできびきび動く)
         //moveVelocity.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
         //moveVelocity.z = Input.GetAxisRaw("Vertical") * moveSpeed;
@@ -44,6 +47,8 @@ public class PlayerController : MonoBehaviour
         //AD入力の時はカメラの横方向に速度を持たせる
         //moveVelocity += cameraRightVector * Input.GetAxis("Horizontal") * moveSpeed;
         moveVelocity.z = cameraForwardVector.z + cameraRightVector.z;
+        this.animator.SetBool("isRun", Input.GetAxis("Vertical") != 0);
+        this.animator.SetBool("isRun2", Input.GetAxis("Horizontal") != 0);
         //Debug.Log(moveVelocity.y);
         //Debug.Log(moveVelocity.z);
         //移動方向を向く
@@ -52,9 +57,14 @@ public class PlayerController : MonoBehaviour
         {
             if (Input.GetButtonDown("Jump"))
             {
+                this.animator.SetBool("isJump", true);
                 //ジャンプ処理
                 Debug.Log("ジャンプ！");
                 moveVelocity.y = jumpPower; //ジャンプの際は上方向に移動させる
+            }
+            else
+            {
+                this.animator.SetBool("isJump", false);
             }
         }
         else
@@ -63,6 +73,7 @@ public class PlayerController : MonoBehaviour
             moveVelocity.y += Physics.gravity.y * Time.deltaTime;
             //Debug.Log(Physics.gravity.y);
         }
+        
         //オブジェクトを動かす
         characterController.Move(moveVelocity * Time.deltaTime);
 
